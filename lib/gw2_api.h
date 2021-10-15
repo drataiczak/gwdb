56a9ed77e05e25fb15c9b937a596ee5d43c8b86a
Devin Rataiczak <drataiczak@gmx.com>
2021-10-14 23:30:30 -0400

    Update endpoints

--- lib/gw2_api.h
+++ lib/gw2_api.h
@@ -1,7 +1,9 @@
 #ifndef GW2_API_H
 #define GW2_API_H
 
-#define BASE_URL "https://api.guildwards.com/"
+#define BASE_URL "https://api.guildwars2.com/"
 #define API_VERS "v2"
+
+#define ACHIEVEMENTS_ENDPOINT "achievements"
 
 #endif
