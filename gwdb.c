e047036b2f7569f8cd633ef54570a959e4a73acb
Devin Rataiczak <drataiczak@gmx.com>
2021-10-14 21:41:41 -0400

    First commit. Basic functions up and running.

---
+++ gwdb.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "discord.h"
+#include "lib/emojis.h"
+
+#define MSG_BUFF_SIZE 1024
+void on_message(
+  struct discord *client,
+  const struct discord_user *bot,
+  const struct discord_message *msg)
+{
+    char *msg_buffer = calloc(1, 256);
+    if(NULL == msg_buffer) {
+      log_info("[!] Failed to create message buffer");
+      return;
+    }
+
+    snprintf(msg_buffer, MSG_BUFF_SIZE, "Hello, %s#%s.", 
+      msg->author->username,
+      msg->author->discriminator);
+
+    struct discord_create_message_params params = { 
+      .content = msg_buffer
+    };
+	
+    if(msg->author->bot || msg->referenced_message) {
+      log_info("msg->author->bot or msg->referenced_message is null");
+      return;
+    }
+
+    discord_create_reaction(client, msg->channel_id, msg->id, 0, THUMBSUP);
+    discord_create_message(client, msg->channel_id, &params, NULL);
+    
+    free(msg_buffer);
+  
+}
+
+void on_ready(
+  struct discord *client, 
+  const struct discord_user *bot) 
+{
+  log_info("Logged in as %s!", bot->username);
+}
+
+int main() {
+  struct discord *client = discord_config_init("config.json");
+
+  discord_set_on_ready(client, &on_ready);
+  discord_set_on_message_create(client, &on_message);
+  discord_run(client);
+
+  discord_cleanup(client);
+  return 0;
+}
---
+++ lib/emojis.h
@@ -0,0 +1,6 @@
+#ifndef EMOJIS_H
+#define EMOJIS_H
+#endif
+
+#define THUMBSUP "\U0001F44D"
+#define THUMBSDOWN "\U0001F44E"
